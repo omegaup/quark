a4fa4fb1734729a415f44b3a192daa00ae8b2805
Freddy Román <me@freddy.mx>
2022-01-16 11:02:45 -0800

    even more tests

--- common/literalinput_test.go
+++ common/literalinput_test.go
@@ -39,3 +39,36 @@ func TestLiteralInput(t *testing.T) {
 	}
 	defer inputRef.Release()
 }
+
+func TestLiteralInputWithExpectedStderr(t *testing.T) {
+	ctx := newTestingContext()
+	defer ctx.Close()
+	dirname, err := ioutil.TempDir("/tmp", t.Name())
+	if err != nil {
+		t.Fatalf("Failed to create temp directory: %q", err)
+	}
+	defer os.RemoveAll(dirname)
+
+	inputManager := NewInputManager(ctx)
+	AplusB, err := NewLiteralInputFactory(
+		&LiteralInput{
+			Cases: map[string]*LiteralCaseSettings{
+				"0": {Input: "1 2", ExpectedOutput: "3", ExpectedValidatorStderr: "err", Weight: big.NewRat(1, 1)},
+				"1": {Input: "2 3", ExpectedOutput: "5", ExpectedValidatorStderr: "err", Weight: big.NewRat(1, 1)},
+			},
+			Validator: &LiteralValidatorSettings{
+				Name: ValidatorNameTokenNumeric,
+			},
+		},
+		dirname,
+		LiteralPersistNone,
+	)
+	if err != nil {
+		t.Fatalf("Failed to create Input: %q", err)
+	}
+	inputRef, err := inputManager.Add(AplusB.Hash(), AplusB)
+	if err != nil {
+		t.Fatalf("Failed to open input: %q", err)
+	}
+	defer inputRef.Release()
+}
